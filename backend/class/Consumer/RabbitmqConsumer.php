27f597291dd4057cb2f41993e1e446b4b98260c4
Jan Nox <jan.nox@pm.me>
2021-12-04 08:04:01 +0100

    Use single quotes

--- backend/class/Consumer/RabbitmqConsumer.php
+++ backend/class/Consumer/RabbitmqConsumer.php
@@ -52,7 +52,7 @@ abstract class RabbitmqConsumer extends Consumer
         $this->logDebug(" [*] - Hostname  is {$env->get("queue>$identifier>host")}");
         $this->logDebug(" [*] - Port      is {$env->get("queue>$identifier>port")}");
         $this->logDebug(" [*] - User name is {$env->get("queue>$identifier>user")}");
-        $this->logDebug(" [*] - Password  is hidden, me Sorree.");
+        $this->logDebug(' [*] - Password  is hidden, me Sorree.');
         $this->logDebug(" [*] - vHost     is {$env->get("queue>$identifier>vhost")}");
         $this->connection = new AMQPStreamConnection(
             $env->get("queue>$identifier>host"),
